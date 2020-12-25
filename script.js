7e5989b150106bd6edbcb31aab253663605a9396
sernili <li.sachse@gmail.com>
2020-12-25 16:57:13 +0100

    change soft scroll on clicks to CSS

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// For soft scroll in navbar
-document.querySelectorAll('.soft-scroll').forEach(anchor => {
-    anchor.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const hrefAttribute = document.querySelector(anchor.getAttribute('href'));
-        hrefAttribute.scrollIntoView({ behavior: 'smooth'});
-    });
-});
-
-
-// Soft scroll with scroll button
-document.querySelector('.scroll-btn').addEventListener("click", () => {
-    document.querySelector('html').style.scrollBehavior = "smooth";
-    
-    setTimeout(() => {
-        document.querySelector('html').style.scrollBehavior = "unset";
-    }, 1000)
-});
-
 // Hide scroll button when on top
 const arrowButton = document.querySelector(".scroll-btn");
 
